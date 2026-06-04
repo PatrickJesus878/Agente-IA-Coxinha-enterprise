790cc97903c4bd9b1bfac17403cc67be6898f67e
PatrickJesus878 <arquivos269@gmail.com>
2026-06-04 11:27:12 -0300

    Update app.py

--- app.py
+++ app.py
@@ -100,7 +100,7 @@ if user_input := str.chat_input("Digite sua mensagem aqui (ex: 'Me passa uma rec
         message_placeholder = str.empty()
         
         completion = client.chat.completions.create(
-            model="llama3-8b-8192",
+            model="llama3-8b-instant",
             messages=str.session_state.messages,
             temperature=0.7,
         )
